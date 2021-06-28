483bfcf4d253263402f8315de08ba9e6bee3228f
Stackbit Code Editor <projects@stackbit.com>
2021-06-28 12:25:39 +0000

    SectionPosts.js: changed

--- src/components/SectionPosts.js
+++ src/components/SectionPosts.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import _ from 'lodash';
 import moment from 'moment-strftime';
-import './font.css'
 import { getPageUrl, Link, withPrefix } from '../utils';
 import CtaButtons from './CtaButtons';
 
